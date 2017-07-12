31caeb7706734c5ecaecaff40a60b63872fb96d7
Darrick <dmckirnan@hotmail.com>
2017-07-12 14:36:36 -0700

    added package.json and new files

---
+++ server/server.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      view: 'login',
+      likesCount: 0,
+    }
+    this.onLike = this.onLike.bind(this);
+  }
+
+  componentDidMount() {
+
+  }
+
+  onLike() {
+    let newLikesCount = this.state.likesCount + 1;
+    this.setState({ likesCount: newLikesCount });
+  }
+
+  loginSuccess() {
+    this.setState({ view: 'dash' });
+  }
+
+
+  render() {
+    return (
+      <div>
+        <p>Hello React!</p>
+        <Counter likesCount = {this.state.likesCount} onLike = {this.onLike}/>
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const config = {
+  entry: {
+    app: './client/index.js'
+  },
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+        test: /\.jsx?/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.scss$/,
+        loaders: ['style-loader', 'css-loader', 'sass-loader']
+      }
+    ],
+  }
+};
+
+module.exports = config;
