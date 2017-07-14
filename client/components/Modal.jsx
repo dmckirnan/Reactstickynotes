012c1f9bbfe39f9c58131cecfaa8cfa3d0711228
Darrick <dmckirnan@hotmail.com>
2017-07-14 15:45:48 -0700

    updated some stylings, changed modal component, removed unnecessary packages in package.json

--- client/components/Modal.jsx
+++ client/components/Modal.jsx
@@ -1,53 +1,23 @@
 import React from 'react';
-import Styles from './styles/Modal.scss';
 
-/*
+const Modal = (
+  <div className="static-modal">
+    <Modal.Dialog>
+      <Modal.Header>
+        <Modal.Title>Modal title</Modal.Title>
+      </Modal.Header>
 
-  Current Role of Modal:
+      <Modal.Body>
+        One fine body...
+      </Modal.Body>
 
-  1) To Provide UI to Edit / Zoom In on Note Content
-  2) Provide Unique UX
+      <Modal.Footer>
+        <Button>Close</Button>
+        <Button bsStyle="primary">Save changes</Button>
+      </Modal.Footer>
 
-  Current Issues:
-  1) Doesn't Work
-  2) Serves Essentially as New View instead of Modal View
-  3) Needs Proper Styling to be a Modal
-
-*/
-
-const Modal = props => {
-  const handleBackgroundClick = e => {
-    if (e.target === e.currentTarget) props.hideModal();
-  };
-
-  const onOk = () => {
-    props.onOk();
-    props.hideModal();
-  };
-
-  const okButton = props.showOk
-    ? (
-      <button
-        onClick={onOk}
-        disabled={props.okDisabled}
-      >
-        {props.okText}
-      </button>
-    ) : null;
-
-  return (
-    <div onClick={handleBackgroundClick}>
-      <header>
-        <h1>{props.title}</h1>
-
-        <button onClick={props.hideModal}>Close</button>
-      </header>
-
-      {props.children}
-
-      {okButton}
-    </div>
-  );
-};
+    </Modal.Dialog>
+  </div>
+);
 
 export default Modal;
